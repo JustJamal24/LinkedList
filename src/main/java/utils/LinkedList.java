e694c7ef36a823bae3e5242ce9b1df77c8fc62af
jeremiahalajemba <JeremiahAlajemba@gmail.com>
2024-03-03 16:20:22 +0000

    Added fields for the Song class Initialised the fields Created Getters and Setters Created and Equals method Created and a Hash Code method Created a toString method
    Added three fields for the LinkedList class
    Initialised the fields
    Created a Size method
    
    Added a get method with validation
    and a loop to get the data at the specified position
    
    Created a Node

--- src/main/java/utils/LinkedList.java
+++ src/main/java/utils/LinkedList.java
@@ -1,6 +1,6 @@
 package utils;
 
-import org.w3c.dom.Node;
+import business.Song;
 
 /**
  *
@@ -22,5 +22,44 @@ public class LinkedList {
                 return this.numberOfSongs;
         }
 
+        public Song get(int position) {
+                if (position < 0 || position >= numberOfSongs) {
+                        throw new IndexOutOfBoundsException("Illegal position has been passed to the LinkedList" + position);
+                }
+                Node current = head;
 
+        for (int i = 0; i < position; i++) {
+            current = current.getNext();
+        }
+        return current.getData();
+
+        }
+
+        protected static class Node {
+                private Song data;
+                private Node next;
+
+                public Node(Song data) {
+                        this.data = data;
+                        this.next = null;
+                }
+
+                public void setNext(Node next) {
+                        this.next = next;
+                }
+
+                public Node getNext() {
+                        return this.next;
+                }
+
+                public void setData(Song data) {
+                        this.data = data;
+                }
+
+                public Song getData() {
+                        return this.data;
+                }
+
+
+        }
 }
